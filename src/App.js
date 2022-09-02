afc5116d41760327b3fbc18117104682e6550d08
DannyDugas <danny.dugas92@gmail.com>
2022-09-01 22:40:48 -0400

    Props, State and Effect
    useState are hooks with getter/setter
    declared as
    const [name, setName] = useState("John");
    used as
    {name} in jsx
    
    setEffect act as callback from a hook's change
    declared as
    useEffect(()=> {
         //* DO THINGS"*//
    }, [name] //*When this hook is updated**//
    
    props are paramters that can be passed from a component to a jsx script

--- src/App.js
+++ src/App.js
@@ -1,34 +1,30 @@
+import { useState, useEffect } from "react";
 import "./App.css";
 
-const Person = (props) => {
-  return (
-    <>
-      <h1>Name : {props.name}</h1>
-      <h1>Last : {props.last}</h1>
-      <h1>Age : {props.age}</h1>
-    </>
-  );
-};
-
 const App = () => {
-  const name = null;
-  const isNameShowing = true;
+  const [counter, setCounter] = useState(500);
+
+  useEffect(() => {
+    alert("You've changed the counter to " + counter);
+  }, [counter]);
 
   return (
     <div className="App">
-      <h1>Hello {isNameShowing ? name : "someone"}, updated in real time</h1>
-      {name ? (
-        <>test</>
-      ) : (
-        <>
-          <h4>
-            <Person name={"Johny"} last="Dooe" age="21" />
-            <Person name={"Boddy"} last="Dooe" age="21" />
-            <Person name={"Shurty"} last="Dooe" age="14" />
-            <Person name={"Cinday"} last="Dooe" age="7" />
-          </h4>
-        </>
-      )}
+      <button
+        onClick={() => {
+          setCounter((prevCount) => prevCount - 1);
+        }}
+      >
+        -
+      </button>
+      <h1>{counter}</h1>
+      <button
+        onClick={() => {
+          setCounter((prevCount) => prevCount + 1);
+        }}
+      >
+        +
+      </button>
     </div>
   );
 };
